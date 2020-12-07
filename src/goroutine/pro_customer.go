91b93c3c42affdde51c797e418babeac8e527c9c
hss <1060067034@qq.com>
2020-12-07 17:42:58 +0800

    [dev]提交

--- src/goroutine/pro_customer.go
+++ src/goroutine/pro_customer.go
@@ -27,11 +27,41 @@ func customer(channel <-chan string)  {
 	}
 }
 
+func producer2(c chan<- int){
+	defer close(c) // 生产结束关闭channel
+
+	for i := 0; i < 10; i++{
+		c <- i // 阻塞，直到消费用完数据c
+		fmt.Println("生产 ",i) // 阻塞，直到生产加入数据c
+	}
+}
+
+func customer2(c <-chan int, f chan <- int)  {
+	for {
+		if v, ok := <-c ;ok {
+			fmt.Println("消费 ",v) // 阻塞，直到生产加入数据c
+			//time.Sleep(1 * time.Second)
+		}else {
+			break
+		}
+	}
+	f <- 1 // 标识消费完了
+}
+
 func main()  {
-	channel := make(chan string)
+	//channel := make(chan string)
+	//
+	//go producer("cat", channel)
+	//go producer("dog", channel)
+	//
+	//customer(channel)
 
-	go producer("cat", channel)
-	go producer("dog", channel)
+	buf := make(chan int)
+	// buf := make(chan int, 10)
+	flg := make(chan int)
+	go producer2(buf)
+	go customer2(buf, flg)
 
-	customer(channel)
+	<- flg // 表面消费结束
+
 }
