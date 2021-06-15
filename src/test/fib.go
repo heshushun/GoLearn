c410cd0c9169cb6b3cdbf36f4ce4fecff3815f7d
hss <1060067034@qq.com>
2021-06-15 18:35:54 +0800

    [dev]Benchmark test

---
+++ src/test/fib.go
@@ -0,0 +1,14 @@
+package test
+
+// 斐波那契数列
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
---
+++ src/test/fib_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+)
+
+func BenchmarkFib10(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fib(10)
+	}
+}
+
+func BenchmarkFib20(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fib(20)
+	}
+}
+
+func BenchmarkSum(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		sum(1, 2)
+	}
+}
