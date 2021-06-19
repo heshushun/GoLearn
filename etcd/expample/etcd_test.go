d0dfa1051a1b5753d4ee7d966025888aca236629
hss <1060067034@qq.com>
2021-06-19 20:25:22 +0800

    [dev]etcd 提交

---
+++ etcd/expample/etcd_test.go
@@ -0,0 +1,138 @@
+package etcd
+
+import (
+	"GoLearn/etcd"
+	"GoLearn/etcd/expample/pb"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+var (
+	dialTimeout    = 5 * time.Second
+	requestTimeout = 2 * time.Second
+	endpoints      = []string{"http://127.0.0.1:2379"}
+)
+
+var (
+	serv = flag.String("service", "hello_service", "service name")
+	port = flag.Int("port", 50001, "listening port")
+	reg  = flag.String("reg", "http://127.0.0.1:2379", "register etcd address")
+)
+
+
+func Test_Etcd(t *testing.T){
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+	log.Println("存储值")
+	if _, err := cli.Put(context.TODO(), "sensors", `{sensor01:{topic:"w_sensor01"}}`); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("获取值")
+	if resp, err := cli.Get(context.TODO(), "sensors"); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("resp: ", resp)
+	}
+	// see https://github.com/coreos/etcd/blob/master/clientv3/example_kv_test.go#L220
+	log.Println("事务&超时")
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	_, err = cli.Txn(ctx).
+		If(clientv3.Compare(clientv3.Value("key"), ">", "abc")). // txn value comparisons are lexical
+		Then(clientv3.OpPut("key", "XYZ")).                      // this runs, since 'xyz' > 'abc'
+		Else(clientv3.OpPut("key", "ABC")).
+		Commit()
+	cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// see https://github.com/coreos/etcd/blob/master/clientv3/example_watch_test.go
+	log.Println("监视")
+	rch := cli.Watch(context.Background(), "", clientv3.WithPrefix())
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+		}
+	}
+}
+
+func Test_HelloWorldClient(t *testing.T){
+	flag.Parse()
+	fmt.Println("serv", *serv)
+	r := etcd.NewResolver(*serv)
+	b := grpc.RoundRobin(r)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	conn, err := grpc.DialContext(ctx, *reg, grpc.WithInsecure(), grpc.WithBalancer(b))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("conn...")
+
+	ticker := time.NewTicker(1 * time.Second)
+	for t := range ticker.C {
+		client := pb.NewGreeterClient(conn)
+		resp, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "world " + strconv.Itoa(t.Second())})
+		if err == nil {
+			fmt.Printf("%v: Reply is %s\n", t, resp.Message)
+		} else {
+			fmt.Println(err)
+		}
+	}
+}
+
+func Test_HelloWorldServer(t *testing.T){
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
+	if err != nil {
+		panic(err)
+	}
+
+	err = etcd.Register(*serv, "127.0.0.1", *port, *reg, time.Second*10, 15)
+	if err != nil {
+		panic(err)
+	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGQUIT)
+	go func() {
+		s := <-ch
+		log.Printf("receive signal '%v'", s)
+		_ = etcd.UnRegister()
+		os.Exit(1)
+	}()
+
+	log.Printf("starting hello service at %d", *port)
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	fmt.Printf("%v: Receive is %s\n", time.Now(), in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
--- go.mod
+++ go.mod
@@ -2,4 +2,41 @@ module GoLearn
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.0 // indirect
+	github.com/coreos/bbolt v1.3.4 // indirect
+	github.com/coreos/etcd v3.3.25+incompatible
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.4.3
+	github.com/google/btree v1.0.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/soheilhy/cmux v0.1.5 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20201229170055-e5319fda7802 // indirect
+	github.com/uber/jaeger-client-go v2.29.1+incompatible
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	go.uber.org/atomic v1.8.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
+	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
+	google.golang.org/grpc v1.35.0
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
+
+replace google.golang.org/grpc v1.35.0 => google.golang.org/grpc v1.26.0
+
+replace github.com/coreos/bbolt v1.3.4 => go.etcd.io/bbolt v1.3.4
