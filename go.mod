c16f2a846b70d11e658c6f9198b30f28a7c9b127
hss <1060067034@qq.com>
2021-06-21 16:03:54 +0800

    [dev]opentracing 添加

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/google/btree v1.0.1 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/jonboulle/clockwork v0.2.2 // indirect
--- tracing/jaeger_test.go
+++ tracing/jaeger_test.go
@@ -1,10 +1,22 @@
 package tracing
 
 import (
+	"GoLearn/etcd"
+	"GoLearn/etcd/expample/pb"
 	"context"
+	"flag"
 	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/opentracing/opentracing-go"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 	"testing"
+	"time"
 )
 
 func Test_Tracer(t *testing.T) {
@@ -18,4 +30,99 @@ func Test_Tracer(t *testing.T) {
 	fmt.Printf("Resp demo: %v \n", r1)
 	fmt.Printf("Resp demo2: %v\n", r2)
 	span.Finish()
+}
+
+var (
+	serv = flag.String("service", "hello_service", "service name")
+	port = flag.Int("port", 50001, "listening port")
+	reg  = flag.String("reg", "http://127.0.0.1:2379", "register etcd address")
+)
+
+const (
+	clientName = "HelloClient"
+	serverName  = "HelloServer"
+)
+
+func Test_TraceClient(t *testing.T){
+	flag.Parse()
+	fmt.Printf("service: %v:%v", *serv, *port)
+
+	// 使用拦截器加入tracer，并创建连接
+	resolver := etcd.NewResolver(*serv)
+	robin := grpc.RoundRobin(resolver)
+	tracer, _ := initJaeger(clientName)
+	chainInter := grpc_middleware.ChainUnaryClient(
+		OpenTracingClientInterceptor(tracer),
+	)
+	dialOpts := []grpc.DialOption{
+		grpc.WithBalancer(robin),
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+		grpc.WithUnaryInterceptor(chainInter),
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	conn, err := grpc.DialContext(ctx, *reg, dialOpts...)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("conn...")
+
+	// 生成client 并发送请求
+	ticker := time.NewTicker(1 * time.Second)
+	for t := range ticker.C {
+		client := pb.NewGreeterClient(conn)
+		resp, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "world " + strconv.Itoa(t.Second())})
+		if err == nil {
+			fmt.Printf("%v: Reply is %s\n", t, resp.Message)
+		} else {
+			fmt.Println(err)
+		}
+	}
+}
+
+func Test_TraceServer(t *testing.T){
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
+	if err != nil {
+		panic(err)
+	}
+
+	err = etcd.Register(*serv, "127.0.0.1", *port, *reg, time.Second*10, 15)
+	if err != nil {
+		panic(err)
+	}
+
+	// 使用拦截器加入tracer
+	tracer, closer := initJaeger(serverName)
+	defer closer.Close()
+	chainInter := grpc_middleware.ChainUnaryServer(
+		OpentracingServerInterceptor(tracer),
+	)
+	servOpts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(chainInter),
+	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGQUIT)
+	go func() {
+		s := <-ch
+		log.Printf("receive signal '%v'", s)
+		_ = etcd.UnRegister()
+		os.Exit(1)
+	}()
+
+	log.Printf("starting hello service at %d", *port)
+	s := grpc.NewServer(servOpts...)
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	fmt.Printf("%v: Receive is %s\n", time.Now(), in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
