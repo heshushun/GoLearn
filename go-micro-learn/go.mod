8c21ba72d7c56b139696f59edb80d34e85401aac
hss <1060067034@qq.com>
2021-07-17 17:23:30 +0800

    [upd]集成断路器Hystrix

--- go-micro-learn/go.mod
+++ go-micro-learn/go.mod
@@ -3,12 +3,11 @@ module go-micro-learn
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+	github.com/gin-gonic/gin v1.7.2
 	github.com/golang/protobuf v1.4.2
 	github.com/micro/go-micro/v2 v2.9.1
-	github.com/nats-io/gnatsd v1.4.1 // indirect
 	github.com/pkg/errors v0.9.1
 	go.mongodb.org/mongo-driver v1.5.4
-	golang.org/dl v0.0.0-20210713194856-38ddc79c2163 // indirect
 	google.golang.org/protobuf v1.23.0
 )
--- go-micro-learn/task-api/main.go
+++ go-micro-learn/task-api/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/registry/etcd"
 	"github.com/micro/go-micro/v2/web"
+	"go-micro-learn/task-api/wrapper/breaker/hystrix"
 	pb "go-micro-learn/task-srv/proto/task"
 	"log"
 	"net/http"
@@ -13,29 +14,60 @@ import (
 
 // task-srv服务的restful api映射
 func main() {
-	g := InitRouter()
+	etcdRegister := etcd.NewRegistry(
+		registry.Addrs("127.0.0.1:2379"),
+	)
+	// 之前我们使用client.DefaultClient注入到pb.NewTaskService中
+	// 现在改为标准的服务创建方式创建服务对象
+	// 但这个服务并不真的运行（我们并不调用他的Init()和Run()方法）
+	// 如果是task-srv这类本来就是用micro.NewService服务创建的服务
+	// 则直接增加包装器，不需要再额外新增服务
+	app := micro.NewService(
+		micro.Name("go.micro.client.task"),
+		micro.Registry(etcdRegister),
+		micro.WrapClient(
+			// 引入hystrix包装器
+			hystrix.NewClientWrapper(),
+		),
+	)
+	taskService := pb.NewTaskService("go.micro.service.task", app.Client())
+
+	// 配置web路由
+	g := InitRouter(taskService)
+	// 这个服务才是真正运行的服务
 	service := web.NewService(
 		web.Name("go.micro.api.task"),
 		web.Address(":8888"),
 		web.Handler(g),
-		web.Registry(etcd.NewRegistry(
-			registry.Addrs("127.0.0.1:2379"),
-		)),
+		web.Registry(etcdRegister),
 	)
-	cli := pb.NewTaskService("go.micro.service.task", client.DefaultClient)
 
-	v1 := g.Group("/task")
+	_ = service.Init()
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InitRouter(taskService pb.TaskService)*gin.Engine{
+	ginRouter := gin.Default()
+	ginRouter.POST("/users", func(context *gin.Context) {
+		context.JSON(http.StatusOK,gin.H{
+			"code": 200,
+			"msg": "请求成功",
+		})
+	})
+	v1 := ginRouter.Group("/task")
 	{
 		v1.GET("/search", func(c *gin.Context) {
 			req := new(pb.SearchRequest)
-			if err := c.ShouldBind(req); err != nil {
+			if err := c.BindQuery(req); err != nil {
 				c.JSON(200, gin.H{
 					"code": "500",
 					"msg":  "bad param",
 				})
 				return
 			}
-			if resp, err := cli.Search(c, req); err != nil {
+			if resp, err := taskService.Search(c, req); err != nil {
 				c.JSON(200, gin.H{
 					"code": "500",
 					"msg":  err.Error(),
@@ -48,19 +80,5 @@ func main() {
 			}
 		})
 	}
-	service.Init()
-	if err := service.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func InitRouter()*gin.Engine{
-	ginRouter := gin.Default()
-	ginRouter.POST("/users", func(context *gin.Context) {
-		context.JSON(http.StatusOK,gin.H{
-			"code": 200,
-			"msg": "请求成功",
-		})
-	})
 	return ginRouter
 }
