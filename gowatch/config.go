5066b64fa6e5ac3efc3ba0ab5aa1850c7b3504d4
hss <1060067034@qq.com>
2021-12-28 18:38:02 +0800

    [dev] gowatch

---
+++ gowatch/config.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v1"
+)
+
+var configFile = "./gowatch/gowatch.yml"
+
+type config struct {
+	//执行的app名字，默认当前目录文字
+	AppName string `yaml:"appname"`
+	//指定ouput执行的程序路径
+	Output string `yaml:"output"`
+	//需要追加监听的文件后缀名字，默认是'.go'，
+	WatchExts []string `yaml:"watch_exts"`
+	//需要追加监听的目录，默认是当前文件夹，
+	WatchPaths []string `yaml:"watch_paths"`
+	//执行时的额外参数
+	CmdArgs []string `yaml:"cmd_args"`
+	//构建时的额外参数
+	BuildArgs []string `yaml:"build_args"`
+	//执行时追加的环境变量
+	Envs []string `yaml:"envs"`
+	//vendor 目录下的文件是否也监听
+	VendorWatch bool `yaml:"vendor_watch"`
+	//不需要监听的目录
+	ExcludedPaths []string `yaml:"excluded_paths"`
+	//需要编译的包或文件,优先使用-p参数
+	BuildPkg string `yaml:"build_pkg"`
+	//在go build 时期接收的-tags参数
+	BuildTags string `yaml:"build_tags"`
+	//程序是否自动运行
+	DisableRun bool `yaml:"disable_run"`
+}
+
+func parseConfig() *config {
+	c := &config{}
+	filename, _ := filepath.Abs(configFile)
+	if !fileExist(filename) {
+		return c
+	}
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
+
+func fileExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
---
+++ gowatch/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"flag"
+	"os"
+	path "path/filepath"
+	"runtime"
+	"strings"
+)
+
+var (
+	cfg      *config
+	curpath  string
+	exit     chan bool
+	output   string
+	buildPkg string
+	cmdArgs  string
+
+	started chan bool
+)
+
+func init() {
+	flag.StringVar(&output, "o", "", "go build output")
+	flag.StringVar(&buildPkg, "p", "", "go build packages")
+	flag.StringVar(&cmdArgs, "args", "", "app run args,separated by commas. like: -args='-host=:8080,-name=demo'")
+}
+
+var ignoredFilesRegExps = []string{
+	`.#(\w+).go`,
+	`.(\w+).go.swp`,
+	`(\w+).go~`,
+	`(\w+).tmp`,
+}
+
+func main() {
+	flag.Parse()
+
+	cfg = parseConfig()
+	curpath, _ = os.Getwd()
+	if cfg.AppName == "" {
+		//app名默认取目录名
+		if output == "" {
+			cfg.AppName = path.Base(curpath)
+		} else {
+			cfg.AppName = path.Base(output)
+		}
+	}
+
+	if output != "" {
+		cfg.Output = output
+	}
+
+	//如果未指定output则为"./appname"
+	if cfg.Output == "" {
+		outputExt := ""
+		if runtime.GOOS == "windows" {
+			outputExt = ".exe"
+		}
+		cfg.Output = "./" + cfg.AppName + outputExt
+	}
+
+	if cmdArgs != "" {
+		cfg.CmdArgs = strings.Split(cmdArgs, ",")
+	}
+
+	//监听的文件后缀
+	cfg.WatchExts = append(cfg.WatchExts, ".go")
+
+	runApp()
+}
+
+func runApp() {
+	var paths []string
+	readAppDirectories(curpath, &paths)
+	//除了当前目录，增加额外监听的目录
+	for _, path := range cfg.WatchPaths {
+		readAppDirectories(path, &paths)
+	}
+
+	files := []string{}
+	if buildPkg == "" {
+		buildPkg = cfg.BuildPkg
+	}
+	if buildPkg != "" {
+		files = strings.Split(buildPkg, ",")
+	}
+	NewWatcher(paths, files)
+	Autobuild(files)
+	for {
+		select {
+		case <-exit:
+			runtime.Goexit()
+		}
+	}
+}
